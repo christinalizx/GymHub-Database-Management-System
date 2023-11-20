38c76308b271df754e10a52f070d3078adbe3050
christinalizx <christinalizx@gmail.com>
2023-11-20 16:35:46 -0500

    Create project

---
+++ src/Model/JDBC.java
@@ -0,0 +1,47 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class JDBC {
+
+  private static final String URL = "jdbc:mysql://database-1.cpqkz8uyycse.us-east-1.rds.amazonaws.com:3306/gymhubdb";
+  private static final String USERNAME = "admin";
+  private static final String PASSWORD = "WKkn3q3YaPWNW8NthFWU";
+  private static JDBC instance;
+  private Connection connection;
+
+  /**
+   * Private constructor to prevent instantiation outside the class
+   **/
+  private JDBC() {
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+          connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+          throw new RuntimeException(e);
+        }
+      }
+    }).start();
+
+  }
+
+  /**
+   * Static method to get the singleton instance
+   **/
+  public static JDBC getInstance() {
+    if (instance == null) {
+      instance = new JDBC();
+    }
+    return instance;
+  }
+}
+
